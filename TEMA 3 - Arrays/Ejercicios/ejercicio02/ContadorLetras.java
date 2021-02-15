c2e7debd6f7e1ef44f28503b3bed3be125f08813
mariacachog <mariacachog@hotmail.com>
2021-02-15 14:07:08 +0100

    Versión 0.1

--- TEMA 3 - Arrays/Ejercicios/ejercicio02/ContadorLetras.java
+++ TEMA 3 - Arrays/Ejercicios/ejercicio02/ContadorLetras.java
@@ -9,41 +9,4 @@ package ejercicio02;
 import java.io.IOException;
 //Siempre que se utilice un método read hay que importar el paquete java.io.IOException
 
-public class ContadorLetras
-{
-	public static void main(String[] args) throws IOException 
-	{
-		int abc[] = new int ['z'-'a'+1];
-		System.out.println("Introduce un texto (pulse enter y, a continuación, Control+d para terminar): ");
-		final char eof = (char) -1;
-		
-		char car = (char) System.in.read();
-		
-		while(car != eof)
-		{
-			//convertimos a minúscula
-			car = Character.toLowerCase(car);
-			
-			//comprobamos si es una letra del abecedario	
-			if(car >= 'a' && car <= 'z')
-				abc[car-'a']++;
-			car = (char) System.in.read(); //Siempre que se utilice un método read hay que importar el paquete java.io.IOException
-		
-		}
-		System.out.println("\n\n");
-		
-		for(char letra = 'a'; letra <= 'z'; letra++)
-		{
-			System.out.print(letra + "  ");
-		}
-		
-		System.out.println("\n");
-		
-		for(int i = 0; i < 'z'-'a'+1; i++)
-		{
-			System.out.print(abc[i]+"  ");
-		}
-
-	}
-
 }
